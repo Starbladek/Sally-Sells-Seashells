80d3ab7da38a48e9acaacfc5697563ba0d3af858
Michael Perez <mperez980089@yahoo.com>
2018-11-16 20:41:57 -0800

    Added organization folders, added Diver and Shell handlers

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    DiverHandler diverHandler;
+    ShellHandler shellHandler;
+
+    void Start()
+    {
+        diverHandler = GetComponent<DiverHandler>();
+        shellHandler = GetComponent<ShellHandler>();
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ShellHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShellHandler : MonoBehaviour
+{
+    public float sellSpeed;
+    public float value;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
