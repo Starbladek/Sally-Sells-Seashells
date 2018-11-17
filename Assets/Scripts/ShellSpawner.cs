b01d7f98b5444839633e78448474dbb094ce2690
Michael Perez <mperez980089@yahoo.com>
2018-11-17 05:49:04 -0800

    Finished shell spawning code

--- Assets/Scripts/ShellSpawner.cs
+++ Assets/Scripts/ShellSpawner.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class ShellSpawner : MonoBehaviour
 {
-    public float shellSpawnTimerLength;
+    public float shellSpawnTimerMinLength;
+    public float shellSpawnTimerMaxLength;
+    float shellSpawnTimerLength;
     float shellSpawnTimer;
 
+    public int maxActiveShells;
+    [HideInInspector]
+    public int currentNumberOfShells;
     public GameObject[] shellPrefabs;
 
     void Start()
     {
+        shellSpawnTimerLength = Random.Range(shellSpawnTimerMinLength, shellSpawnTimerMaxLength);
         shellSpawnTimer = shellSpawnTimerLength;
     }
 
@@ -19,21 +25,42 @@ public class ShellSpawner : MonoBehaviour
         shellSpawnTimer -= Time.deltaTime;
         if (shellSpawnTimer <= 0)
         {
-            int shellNum = 0;
-            float randomValue = Random.Range(0f, 100f);
+            if (currentNumberOfShells < maxActiveShells)
+            {
+                currentNumberOfShells++;
+                float randomValue = Random.Range(0f, 100f);
 
-            if (randomValue < 90)
-                shellNum = 0;
-            else if (randomValue >= 90 && randomValue < 95)
-                shellNum = 1;
-            else if (randomValue >= 95 && randomValue < 99)
-                shellNum = 2;
-            else
-                shellNum = 3;
+                if (randomValue < 90)
+                {
+                    GameObject newShell = Instantiate(shellPrefabs[0],
+                    new Vector2(-10f + Random.Range(0, 3f), 7f - Random.Range(0, 4f)),
+                    Quaternion.identity);
+                    newShell.GetComponent<NormalShell>().shellSpawner = this;
+                }
+                else if (randomValue >= 90 && randomValue < 95)
+                {
+                    GameObject newShell = Instantiate(shellPrefabs[1],
+                    new Vector2(-10f + Random.Range(0, 3f), 7f - Random.Range(0, 4f)),
+                    Quaternion.identity);
+                    newShell.GetComponent<HermitShell>().shellSpawner = this;
+                }
+                else if (randomValue >= 95 && randomValue < 99)
+                {
+                    GameObject newShell = Instantiate(shellPrefabs[2],
+                    new Vector2(-10f + Random.Range(0, 3f), 7f - Random.Range(0, 4f)),
+                    Quaternion.identity);
+                    newShell.GetComponent<ConchShell>().shellSpawner = this;
+                }
+                else
+                {
+                    GameObject newShell = Instantiate(shellPrefabs[3],
+                    new Vector2(-10f + Random.Range(0, 3f), 7f - Random.Range(0, 4f)),
+                    Quaternion.identity);
+                    newShell.GetComponent<Starfish>().shellSpawner = this;
+                }
+            }
 
-            Instantiate(shellPrefabs[shellNum],
-                new Vector2(-10f + Random.Range(0, 3f), 7f - Random.Range(0, 4f)),
-                Quaternion.identity);
+            shellSpawnTimerLength = Random.Range(shellSpawnTimerMinLength, shellSpawnTimerMaxLength);
             shellSpawnTimer += shellSpawnTimerLength;
         }
     }
--- Assets/Scripts/Shells/HermitShell.cs
+++ Assets/Scripts/Shells/HermitShell.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 
 public class HermitShell : MonoBehaviour
 {
-    public int shellValue;
     bool clicked;
+    public int shellValue;
+
+    public float minLifeLength;
+    public float maxLifeLength;
+    float lifeTimerLength;
+    float lifeTimer;
+
+    [HideInInspector]
+    public ShellSpawner shellSpawner;
+
+
+
+    void Start()
+    {
+        lifeTimerLength = Random.Range(minLifeLength, maxLifeLength);
+        lifeTimer = lifeTimerLength;
+    }
+
+    void Update()
+    {
+        if (!clicked)
+        {
+            lifeTimer -= Time.deltaTime;
+            if (lifeTimer <= 0)
+            {
+                shellSpawner.currentNumberOfShells--;
+                Destroy(gameObject);
+            }
+        }
+    }
 
     void OnMouseDown()
     {
@@ -16,6 +45,7 @@ public class HermitShell : MonoBehaviour
             LeanTween.alpha(gameObject, 0, 1);
             LeanTween.scale(gameObject, transform.localScale * 3, 1).setEase(LeanTweenType.easeOutExpo).setOnComplete(() =>
             {
+                shellSpawner.currentNumberOfShells--;
                 Destroy(gameObject);
             });
         }
